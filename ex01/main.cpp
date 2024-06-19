eb025654b97ac411092e0d04b316d79f9018ecf2
TomTris <quoctrido.jb@gmail.com>
2024-06-19 12:03:04 +0200

    changed

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -41,45 +41,79 @@
 
 // 	std::cout << "------" << std::endl;
 // }
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+// template < typename T>
+// void    square(T &a)
+// {
+//     (void)a;
+//     std::cout << "a" << std::endl;
+// }
+// template < typename T>
+// void    add_3(T &a)
+// {
+//     a += 3.0;
+// }
+// int main()
+// {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     iter(arr, 5, square<int>);
+//     for (int i = 0; i < 5; i++)
+//     {
+//         std::cout << arr[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     double arr1[3] = {1.2, 2.3, 3.1};
+//     iter(arr1, 3, square<double>);
+//     for (int i = 0; i < 3; i++)
+//     {
+//         std::cout << arr1[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     iter(arr1, 3, add_3< double>);
+//     for (int i = 0; i < 3; i++)
+//     {
+//         std::cout << arr1[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     int arr2[5] = {1, 2, 3, 4, 5};
+//     iter(arr2, 5, add_3<int>);
+//     for (int i = 0; i < 5; i++)
+//     {
+//         std::cout << arr2[i] << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
-template < typename T>
-void    square(T &a)
-{
-    (void)a;
-    std::cout << "a" << std::endl;
-}
-template < typename T>
-void    add_3(T &a)
-{
-    a += 3.0;
-}
-int main()
-{
-    int arr[5] = {1, 2, 3, 4, 5};
-    iter(arr, 5, square<int>);
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-    double arr1[3] = {1.2, 2.3, 3.1};
-    iter(arr1, 3, square<double>);
-    for (int i = 0; i < 3; i++)
-    {
-        std::cout << arr1[i] << " ";
-    }
-    std::cout << std::endl;
-    iter(arr1, 3, add_3< double>);
-    for (int i = 0; i < 3; i++)
-    {
-        std::cout << arr1[i] << " ";
-    }
-    std::cout << std::endl;
-    int arr2[5] = {1, 2, 3, 4, 5};
-    iter(arr2, 5, add_3<int>);
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << arr2[i] << " ";
-    }
-    std::cout << std::endl;
-}
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+
+//   return 0;
+// }
